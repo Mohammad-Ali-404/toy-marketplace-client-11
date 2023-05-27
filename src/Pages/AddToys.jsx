28b70c99014e194ec286f37bdda154d5a8e9f526
wpmohammad1 <alimohammadjhd@gmail.com>
2023-05-27 11:32:57 +0600

    add toy data send to backend

--- src/Pages/AddToys.jsx
+++ src/Pages/AddToys.jsx
@@ -1,14 +1,39 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const AddToys = () => {
+    const handleAddToy = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const seller = form.seller.value;
+        const email = form.email.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photoUrl = form.photo.value;
+
+        const newtoy = {name, quantity, seller, email, category, details, photoUrl};
+        console.log(newtoy)
+        // send added toy data
+        fetch('http://localhost:5000/addtoy', {
+            method:'POST',
+            headers:{
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newtoy)
+        })
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
     return (
         // <div>
         //     <h1 className='text-4xl text-center font-extrabold'>Add Toy</h1>
         // </div>
         <div className="bg-[#F4F3F0] p-24">
             <h2 className="text-4xl mb-8 text-center font-extrabold">Add a Toy</h2>
-            <form >
+            <form onSubmit={handleAddToy}>
                 {/* form name and quantity row */}
                 <div className="md:flex mb-8">
                     <div className="form-control md:w-1/2">
@@ -16,7 +41,7 @@ const AddToys = () => {
                             <span className="label-text">Toy Brand Name</span>
                         </label>
                         <label className="input-group">
-                            <input type="text" name="name" placeholder="Coffee Name" className="input input-bordered w-full" />
+                            <input type="text" name="name" placeholder="Toy Name" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-4">
@@ -35,7 +60,7 @@ const AddToys = () => {
                             <span className="label-text">Seller Name</span>
                         </label>
                         <label className="input-group">
-                            <input type="text" name="supplier" placeholder="Supplier Name" className="input input-bordered w-full" />
+                            <input type="text" name="seller" placeholder="Supplier Name" className="input input-bordered w-full" />
                         </label>
                     </div>
                     <div className="form-control md:w-1/2 ml-4">
